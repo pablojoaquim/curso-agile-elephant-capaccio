9b25515efd1a1d97dfd5f2f3d1c35f1913507345
pablojoaquim <pablo.joaquim@famar.com>
2021-08-04 01:56:52 -0300

    story 4

--- elephant.rb
+++ elephant.rb
@@ -4,7 +4,7 @@ class Elephant
     end
 
     def calcularRecibo(cant=0, precio=0, estado="XX")
-        @val = cant.to_i * precio.to_i + 2
+        @val = ((cant.to_i * precio.to_i) * 1.1).round(4)
         puts @val
 
     end
