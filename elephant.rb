9270a57ec26b3218c5b2c5a59ca448beba756646
pablojoaquim <pablo.joaquim@famar.com>
2021-08-04 01:48:45 -0300

    story 1

---
+++ elephant.rb
@@ -0,0 +1,13 @@
+class Elephant
+
+    def initialize()
+    end
+
+    def calcularRecibo(cant=0, precio=0, estado="XX")
+        puts 52
+
+    end
+end
+
+elephant= Elephant.new 
+elephant.calcularRecibo(ARGV[0], ARGV[1], ARGV[2])
